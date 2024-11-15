c1a14ded85c3c458b937a5025239af88b4116502
Mohamed Fathalla <125223665+MohammedFathalla@users.noreply.github.com>
2024-11-15 23:41:24 +0200

    Delete ROS directory

--- ROS/navigation_pkg/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mohamed/work_ws/src/Graduation/navigation_pkg
-# Build directory: /home/mohamed/work_ws/src/Graduation/navigation_pkg/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
